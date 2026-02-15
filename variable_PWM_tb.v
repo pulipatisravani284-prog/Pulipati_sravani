1baec38d0088d50822076d36313282efbaa84998
pulipatisravani284-prog <pulipatisravani284@gmail.com>
2026-02-15 20:38:12 +0530

    Variable_PWM_tb for PWM testing
    Add a testbench for Variable_PWM module with duty cycle changes.

--- variable_PWM_tb.v
+++ variable_PWM_tb.v
@@ -1,1 +1,29 @@
+`timescale 1ns/1ns
+module Variable_PWM_tb;
+reg [7:0] Dutycycle;
+reg clk;
+wire [7:0] counter;
+wire pwmout;
 
+Variable_PWM dut(Dutycycle, clk, counter, pwmout);
+
+initial begin
+    clk = 1'b1;
+end
+
+always #10 clk <= ~clk; 
+
+initial begin
+    #0 Dutycycle = 0;
+    #2000 Dutycycle = 10;
+    #2000 Dutycycle = 20; 
+    #2000 Dutycycle = 30; 
+    #2000 Dutycycle = 40; 
+    #2000 Dutycycle = 50; 
+    #2000 Dutycycle = 60; 
+    #2000 Dutycycle = 70; 
+    #2000 Dutycycle = 80; 
+    #2000 Dutycycle = 90; 
+    #2000 $finish; 
+end
+endmodule
